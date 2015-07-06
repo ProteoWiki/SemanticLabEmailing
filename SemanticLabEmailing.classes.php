64f44e71e88b443c4792d637ae8a6ab8fc1f64bb
toniher <toniher@cau.cat>
2015-07-06 17:34:40 +0200

    update definition

--- SemanticLabEmailing.classes.php
+++ SemanticLabEmailing.classes.php
@@ -28,7 +28,6 @@ define( 'CLOSED_EXPERIMENT', 12 );
 define( 'WEEK_EXTENSION', 13 );
 
 
-
 /**
  * This class handles the creation and sending of notification emails.
  */
@@ -615,33 +614,35 @@ class SemanticLabEmailingMailer {
 			}
 	}
 
-
 	/**
-	 * This function returns to results of a certain query
-	 * Thank you Yaron Koren for advices concerning this code
-	 * @param $query_string String : the query
-	 * @param $properties_to_display array(String): array of property names to display
-	 * @param $display_title Boolean : add the page title in the result
-	 * @return TODO
-	 */
+	* This function returns to results of a certain query
+	* Thank you Yaron Koren for advices concerning this code
+	* @param $query_string String : the query
+	* @param $properties_to_display array(String): array of property names to display
+	* @param $display_title Boolean : add the page title in the result
+	* @return TODO
+	*/
 	static function getQueryResults( $query_string, $properties_to_display, $display_title ) {
+	
 		// We use the Semantic MediaWiki Processor
 		// $smwgIP is defined by Semantic MediaWiki, and we don't allow
 		// this file to be sourced unless Semantic MediaWiki is included.
 		global $smwgIP;
-		include_once( $smwgIP . "/includes/SMW_QueryProcessor.php" );
-
+		
+		if ( file_exists( $smwgIP . "/includes/SMW_QueryProcessor.php") ) {
+			include_once( $smwgIP . "/includes/SMW_QueryProcessor.php" );
+		} else {
+			include_once( $smwgIP . "/includes/query/SMW_QueryProcessor.php" );
+		}
 		$params = array();
 		$inline = true;
 		$printlabel = "";
 		$printouts = array();
-
 		// add the page name to the printouts
 		if ( $display_title ) {
 			$to_push = new SMWPrintRequest( SMWPrintRequest::PRINT_THIS, $printlabel );
 			array_push( $printouts, $to_push );
 		}
-
 		// Push the properties to display in the printout array.
 		foreach ( $properties_to_display as $property ) {
 			if ( class_exists( 'SMWPropertyValue' ) ) { // SMW 1.4
@@ -651,7 +652,6 @@ class SemanticLabEmailingMailer {
 			}
 			array_push( $printouts, $to_push );
 		}
-
 		if ( version_compare( SMW_VERSION, '1.6.1', '>' ) ) {
 			SMWQueryProcessor::addThisPrintout( $printouts, $params );
 			$params = SMWQueryProcessor::getProcessedParams( $params, $printouts );
@@ -660,10 +660,8 @@ class SemanticLabEmailingMailer {
 		else {
 			$format = 'auto';
 		}
-		
 		$query = SMWQueryProcessor::createQuery( $query_string, $params, $inline, $format, $printouts );
 		$results = smwfGetStore()->getQueryResult( $query );
-
 		return $results;
 	}
 
