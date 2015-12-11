21eab0823683dfb2ad4b6b4d7a72801c1904535e
toniher <toniher@cau.cat>
2015-12-11 12:23:36 +0100

    creation of page

--- SemanticLabEmailing.pagecreator.classes.php
+++ SemanticLabEmailing.pagecreator.classes.php
@@ -12,20 +12,53 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 class SemanticLabEmailingPageCreator {
 
 
-	public static function create( $title_text, $template, $prefix ) {
+	public static function create( $titleText, $template, $prefix ) {
 
 		// Template page
-		$origin = "MediaWiki:SemanticLabEmailing-$template";
+		$templatePage = "MediaWiki:SemanticLabEmailing-$template";
 
 		//Final page
-		$pagename = $prefix.":".$title_text;
+		$pagename = $prefix.":".$titleText;
 
 		// Read template page
+		$templateID = Title::newFromText($templatePage)->getArticleID(); //Get the id for the article called Test_page
+		$templateArticle = Article::newFromId($templateID); //Make an article object from that id
+		$templateText = $templateArticle->getRawText();
+
+		if (empty($templateText)) {
+			return false;
+		}
 
 		// Substitute
+		$finalText = self::subsText( $templateText, $titleText );
 
-		// Create page
+		// Create page. Since only one, let's do straight
+		$article = new Article( $pagename );
+
+		$content = \ContentHandler::makeContent(
+				$titleText,
+				$article->getTitle(),
+				CONTENT_MODEL_WIKITEXT
+		);
+
+		$article->doEditContent( $content, $edit_summary );
+
+		// Let's return page name
+		return $article->getTitle;
 
 	}
+
+	private static function subsText( $templateText, $titleText ) {
+
+		// Only one variable
+		$templateEnd = str_replace( "#1", $titleText, $templateText );
+
+		$templateEnd = str_replace("??", "{{", $templateEnd);
+		$templateEnd = str_replace("!!", "}}", $templateEnd);
+
+		return $templateEnd;
+
+	}
+
 
 }
