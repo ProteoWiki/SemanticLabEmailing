86a6e817f5440238fb3b6c76d2f24e65b56ecb45
toniher <toniher@cau.cat>
2016-02-11 13:43:45 +0100

    adding structure

--- SemanticLabEmailing.special.php
+++ SemanticLabEmailing.special.php
@@ -6,14 +6,37 @@ class SpecialSemanticLabEmailingFeedback extends SpecialPage {
 
 	public function execute($par) {
 
+		global $wgOut;
 		global $wgSemanticLabEmailingCreatePage;
 
 		$this->setHeaders();
 
 		// Get this info from 
-		$optionsArray = array();
+		$formDescriptorUpload = array();
+
+		$htmlForm = new HTMLForm( $formDescriptorUpload, 'semanticlabemailing_form' );
+
+		$htmlForm->setSubmitText( 'Send' ); //TODO: Change button label
+
+		/* We set a callback function */
+		$htmlForm->setSubmitCallback( array( 'SpecialSemanticLabEmailingFeedback', 'processInput' ) );  # Call processInput() in SpecialBioParser on submit
+
+		$htmlForm->suppressReset(false); # Get back reset button
+
+		$wgOut->addHTML( "<div class='semanticlabemailing_section'>" );
+		$htmlForm->show(); # Displaying the form
+		$wgOut->addHTML( "</div>" );
 
 	}
 
+	static function processInput( $formData ) {
 
+		global $wgSemanticLabEmailingCreatePage;
+
+		// TODO: Parse $formData
+
+		// TODO: API call
+
+		return "DONE";
+	}
 }
